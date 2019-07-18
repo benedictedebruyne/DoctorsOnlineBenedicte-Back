d1e974b74d7d54731dd13bbb3cd4bf02e12898ae
Benedicte Debruyne <bdebruyne@gmail.com>
2019-07-18 11:41:52 +0200

    Commit initial du back

---
+++ src/main/java/be/businesstraining/doctorsonlinebeneback/DoctorsonlinebenebackApplication.java
@@ -0,0 +1,13 @@
+package be.businesstraining.doctorsonlinebeneback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsonlinebenebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorsonlinebenebackApplication.class, args);
+    }
+
+}
